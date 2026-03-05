bb94dc9255e60e496be681d41485601d8d876d1b
bornyonline <113050459+bornyonline@users.noreply.github.com>
2026-03-04 19:16:27 -0500

    Initial Commit

---
+++ HealthScope/HealthScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthScopeApp.swift
+//  HealthScope
+//
+//  Created by eduardo villani on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
